b906b7c84465dc54a0249268ed9403efad164554
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-12 21:54:14 +0900

    Cannot fix no-returnable bug

--- app/src/main/java/com/gardenlab/doitmission10/Fragment1.java
+++ app/src/main/java/com/gardenlab/doitmission10/Fragment1.java
@@ -1,5 +1,6 @@
 package com.gardenlab.doitmission10;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,65 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class Fragment1 extends Fragment {
+    ViewPager pager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment1, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+        pager= rootView.findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+
+        MyViewPagerAdapter mPagerAdapter = new MyViewPagerAdapter(getChildFragmentManager());
+        Fragment view1 = new View1();
+        Fragment view2 = new View2();
+        Fragment view3 = new View3();
+        mPagerAdapter.addItem(view1);
+        mPagerAdapter.addItem(view2);
+        mPagerAdapter.addItem(view3);
+
+        pager.setAdapter(mPagerAdapter);
+        return rootView;
     }
+
+
+    class MyViewPagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> views = new ArrayList<>();
+
+        MyViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return views.get(position);
+        }
+
+        public void addItem(Fragment view){
+            views.add(view);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
 }
+
---
+++ app/src/main/java/com/gardenlab/doitmission10/View1.java
@@ -0,0 +1,18 @@
+package com.gardenlab.doitmission10;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class View1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view1, container, false);
+    }
+}
